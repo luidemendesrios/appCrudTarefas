49d1ab21b7ae476c2432a7ed7aae272f6eee1ab6
Luide Mendes Rios <luidemendesrios@gmail.com>
2023-12-05 16:52:33 -0400

    Primeiro commit do app Crud_tarefas

--- lib/business_logic/tarefa_business_logic.dart
+++ lib/business_logic/tarefa_business_logic.dart
@@ -3,13 +3,11 @@ import 'package:flutter/material.dart';
 class Tarefa {
 
   late String nome;
-  //late bool concluida;
   late DateTime data;
   late ValueNotifier<bool> concluida;
 
   Tarefa(String nome) {
     this.nome = nome;
-    //this.concluida = false;
     this.concluida = ValueNotifier<bool>(false);
     this.data = DateTime.now();
   }
